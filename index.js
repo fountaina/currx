01fd8d7d31cf2d25954e7f21b902210ec2405c88
Fountain <fountainaigbona@gmail.com>
2024-10-16 12:41:48 +0100

    Basic backend functionality working

---
+++ index.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+// Source all static files from the "public" folder
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+    res.render("index.ejs");
+})
+
+// Initiates port 
+app.listen(port, () => {
+    console.log("Server running on port " + port);
+});
