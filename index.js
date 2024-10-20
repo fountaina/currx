327206c397bb20c5bd1e5147ef01acb9296fe6db
Fountain <fountainaigbona@gmail.com>
2024-10-20 20:43:28 +0100

    integrate currency list from api to frontend

--- index.js
+++ index.js
@@ -1,14 +1,33 @@
 import axios from "axios";
 import express from "express";
+import bodyParser from "body-parser";
 
 const app = express();
 const port = 3000;
+const API_URL = "https://api.frankfurter.app/"
 
 // Source all static files from the "public" folder
 app.use(express.static("public"));
 
-app.get("/", (req, res) => {
-    res.render("index.ejs");
+app.use(bodyParser.urlencoded({ extended: true}));
+
+app.get("/", async (req, res) => {
+
+    try {
+        const response = await axios.get(`${API_URL}currencies`);
+        const currencies = response.data;
+        console.log(currencies);
+        res.render("index.ejs", {currencies: currencies});
+    } catch (error) {
+        console.error("Failed to get data: " + error.message);
+    }
+    
+});
+
+app.post("/submit", async (req, res) => {
+    const rates = await axios.get(`${API_URL}latest?base=${req.body.base}`);
+
+    console.log(rates.data);
 });
 
 // Initiates port 
