cb1ee7532e9dc0ebf7ce37819f77d0ec0a561065
Fountain <fountainaigbona@gmail.com>
2024-10-22 10:59:28 +0100

    logic modification to the convert function

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ app.use(bodyParser.urlencoded({ extended: true}));
 
 app.get("/", async (req, res) => {
 
+    
     try {
         const response = await axios.get(`${API_URL}currencies`);
         const currencies = response.data;
@@ -20,14 +21,28 @@ app.get("/", async (req, res) => {
         res.render("index.ejs", {currencies: currencies});
     } catch (error) {
         console.error("Failed to get data: " + error.message);
+        res.render("index.ejs");
     }
     
 });
 
 app.post("/submit", async (req, res) => {
-    const rates = await axios.get(`${API_URL}latest?base=${req.body.base}`);
+    // const rates = await axios.get(`${API_URL}latest?base=${req.body.base}`);
 
-    console.log(rates.data);
+    console.log(req.body);
+    
+    // converts client's amount between currency pairs.
+    async function convert(from, to, amount) {
+        const response = await axios.get(`https://api.frankfurter.app/latest?base=${from}&symbols=${to}`);
+        const data = response.data;
+        const convertedAmount = (amount * data.rates[to]).toFixed(2);
+        console.log("Converted amount value: " + convertedAmount);
+        return Number(convertedAmount);
+    }
+    // Usage
+    const newAmount = await convert(`${req.body.base}`, `${req.body.new}`, Number(req.body.amount));
+    console.log("New Amount: " + newAmount);
+
 });
 
 // Initiates port 
